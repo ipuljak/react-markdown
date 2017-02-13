031ef6db0b10ea645761a1b5d18483377ec9abb9
Ivan <ivanpulj@gmail.com>
2017-02-13 01:37:58 -0500

    Added README file and some code documentation

---
+++ README.md
@@ -0,0 +1,11 @@
+#Markdown Previewer
+
+A markdown previewer written in React that allows you to preview the converted HTML output of your markdown as you type. To read more about markdown, check [this reference](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) out.
+
+###Live Demo###
+
+You can try out the live demo [here](https://www.puljak.ca/projects/react/markdown).
+
+###About Me###
+
+For more information about me, visit my website at [puljak.ca](https://puljak.ca)!
--- index.js
+++ index.js
@@ -2,46 +2,55 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import marked from 'marked';
 
+/**
+ *  Marked class component responsible for displaying a textarea for user input
+ *  and beside it the markdown preview of what the user is inputting
+ */
 class Marked extends Component {
-    constructor(props) {
-        super(props);
-        
-        var textArea = 'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[Herman Fassett](https://freecodecamp.com/hermanfassett)*';
-    
-        this.state = {
-            text: textArea
-        };
-    }
+  constructor(props) {
+    super(props);
 
-    updateMarked(text) {
-        this.setState({text: text});
-    }
+    // Define some placeholder text for the textarea box
+    var textArea = 'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[Herman Fassett](https://freecodecamp.com/hermanfassett)*';
 
-    convertToHTML(text) {
-        var textHTML = marked(text, {sanitize: true});
-        var markupHTML = {
-          __html: textHTML
-        };
-        return markupHTML;
-    }
+    // Set the state to be the user's textarea box
+    this.state = {
+      text: textArea
+    };
+  }
 
-    render() {
-        return (
-          <div className="row">
-            <div className="col-md-6">
-                <textarea 
-                    className="form-control" 
-                    rows="30" 
-                    onChange={event => this.updateMarked(event.target.value)}
-                    value={this.state.text}>
-                </textarea>
-            </div>
-            <div className="col-md-6">
-                <div dangerouslySetInnerHTML={this.convertToHTML(this.state.text)}></div>
-            </div>
-          </div>
-        );
-    }
+  // Update the markdown page
+  updateMarked(text) {
+    this.setState({ text: text });
+  }
+
+  // Convert the markdown to HTML
+  convertToHTML(text) {
+    var textHTML = marked(text, { sanitize: true });
+    var markupHTML = {
+      __html: textHTML
+    };
+    return markupHTML;
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <div className="col-md-6">
+          <textarea
+            className="form-control"
+            rows="30"
+            onChange={event => this.updateMarked(event.target.value)}
+            value={this.state.text}>
+          </textarea>
+        </div>
+        <div className="col-md-6">
+          <div dangerouslySetInnerHTML={this.convertToHTML(this.state.text)}></div>
+        </div>
+      </div>
+    );
+  }
 }
 
+// React entry point into the DOM
 ReactDOM.render(<Marked />, document.getElementById("inputMarked"));
